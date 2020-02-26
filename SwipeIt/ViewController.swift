70bf946c71f2f5b22fa73c01476a74b4589bcfdf
nnarayanan <nithindev.narayanan@accenture.com>
2020-02-26 12:14:29 +0530

    Initial Commit

---
+++ SwipeIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeIt
+//
+//  Created by nithindev.narayanan on 26/02/20.
+//  Copyright © 2020 nithindev.narayanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
