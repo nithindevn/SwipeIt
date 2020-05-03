76d06b225763d939e99e7511ba74145a4fb9bbdf
nnarayanan <nithindev.narayanan@accenture.com>
2020-05-03 22:40:10 +0530

    Initial commit

--- SwipeIt/ViewController.swift
+++ SwipeIt/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swipeView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(didSwipeView(sender:)))
+        swipeView.addGestureRecognizer(panGesture)
     }
 
+    @objc func didSwipeView(sender: UIPanGestureRecognizer) {
+        let panView = sender.view
+        let point = sender.translation(in: view)
+        panView?.center = CGPoint(x: view.center.x  + point.x, y: view.center.y + point.y)
+        if sender.state == UIGestureRecognizer.State.ended {
+            UIView.animate(withDuration: 0.2) {
+                panView?.center = self.view.center
+            }
+        }
+    }
 
 }
-
